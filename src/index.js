32b8c7d17a2fd45a39e66327fb37f3db0786371b
Mikhail007008 <kasseta136@mail.ru>
2021-10-21 21:03:23 +0300

    build(diplom): made a slider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@
 
 import comeModal from './modules/modal';
 import scrolling from './modules/scrollTo';
+import slider from './modules/slider';
 
 //ScrollTo
 scrolling();
 //Modal callback
 comeModal();
+//Slider
+slider();
---
+++ src/modules/slider.js
@@ -0,0 +1,40 @@
+const slider = () =>{
+	const slide = document.querySelectorAll('.item'),
+	sliderTable = document.querySelectorAll('.table');
+
+	let currentSlide = 0,
+		interval;
+
+	sliderTable.forEach((el) =>{
+		el.style = `
+		opacity: 0.9;
+		visibility: visible;`;
+	});
+
+	const prev = (elem, index) =>{
+		elem[index].style.display = 'none';
+	};
+
+	const next = (elem, index) =>{
+		elem[index].style.display = 'block';
+	};
+
+	const autoPlay = () =>{
+		prev(slide, currentSlide);
+		currentSlide++;
+		if(currentSlide >= slide.length){
+			currentSlide = 0;
+		}
+		next(slide, currentSlide);
+	};
+
+	const startSlide = (time = 3000) =>{
+		interval = setInterval(autoPlay, time);
+	};
+
+	startSlide();
+};
+
+export default slider;
+
+
