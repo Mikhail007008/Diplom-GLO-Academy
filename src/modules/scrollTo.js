4f1ade90c6143cf12df15facc9f41bd1af9c1bb8
Mikhail007008 <kasseta136@mail.ru>
2021-10-23 14:05:06 +0300

    build(diplom): update scrollUp

---
+++ src/modules/scrollTo.js
@@ -0,0 +1,44 @@
+const scrolling = () =>{
+	const menu = document.querySelector('.top-menu'),
+		menuItems = menu.querySelectorAll('li>a'),
+		btnUp = document.querySelector('.up');
+	btnUp.style.visibility = 'hidden';
+
+	document.addEventListener('scroll', ()=>{
+		let scrollValue = window.scrollY;
+		
+		if(scrollValue > 600){
+			btnUp.style.visibility = 'visible';
+		}else{
+			btnUp.style.visibility = 'hidden';
+		}
+	
+	});
+
+	const scrolTo = (element) => {
+		window.scroll({
+			left: 0,
+			top: element.offsetTop,
+			behavior: 'smooth'
+		});
+	};
+
+	document.addEventListener('click', (event) =>{
+		let target = event.target;
+
+		if(btnUp === target){
+			scrolTo(menu);
+		}
+
+		menuItems.forEach((item) =>{
+			event.preventDefault();
+
+			if(item === target){
+				let nameToScroll = item.getAttribute('href');
+				scrolTo(document.querySelector(nameToScroll));
+			}
+		});
+	});
+};
+
+export default scrolling;
